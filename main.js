ebfd7164af2a3019d02c7d535d92c8dfb3535d5c
unobliged <brian.ortega@gmail.com>
2013-01-26 12:19:04 -0500

    Updated with some jquery/javascript for projects

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+	$('div#projects>div>a>img').on('click', function(event){
+		if ($(this).attr('src').indexOf('tn_') >= 0) {
+			var new_src = $(this).attr('src').replace('tn_','');
+			$(this).toggleClass('big').attr('src', new_src);
+			$(this).next().toggleClass('hidden big');			
+			$('body').append('<div></div>');
+			$('div:last').attr('id', 'overlay');
+		} else {
+			var new_src = 'tn_' + $(this).attr('src');
+			$(this).toggleClass('big').attr('src', new_src);
+			$(this).next().toggleClass('hidden big');
+			$('div#overlay').remove();
+		}
+	});
+	
+
+	
+	
+});
