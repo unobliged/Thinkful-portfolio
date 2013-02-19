62183036d3f33e76b4f49b5e4020291950417d44
unobliged <brian.ortega@gmail.com>
2013-02-18 21:16:26 -0500

    Added Shopping List App, Google Analytics

--- main.js
+++ main.js
@@ -17,7 +17,19 @@ $(document).ready(function() {
 	
 	$('div#projects>div>a>img').hover(function(){
 		$(this).parent().effect('highlight', {color: '#8a8db6'}, 'fast');
-	});
+	});	
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-38606240-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+
 	
 	
 });
